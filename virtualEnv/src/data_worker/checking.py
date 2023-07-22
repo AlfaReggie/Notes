fa8468c0f01d89cc61138276dc54379382d73f4e
AlfaReggie <107858720+AlfaReggie@users.noreply.github.com>
2023-07-23 02:50:06 +0300

    Update checking.py
    fix

--- virtualEnv/src/data_worker/checking.py
+++ virtualEnv/src/data_worker/checking.py
@@ -14,7 +14,7 @@ class Checking:
     def check_int(self, numbStrVal: int):
         checkNum = input(f"\nEnter {self.val[numbStrVal]}: ")
         if checkNum.replace("-", '').isdigit():
-            if int(checkNum) < 0:
+            if int(checkNum) <= 0:
                 print("Error! Can't negative!")
                 return Checking.check_int(self, numbStrVal)
             return int(checkNum)
